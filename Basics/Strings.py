61babe4f0ff64ae6cccdac9b8f3588def500ec35
Chanith Adikari <112007446+Chanith27@users.noreply.github.com>
2025-07-01 10:47:42 +0530

    Update Strings.py

--- Basics/Strings.py
+++ Basics/Strings.py
@@ -1,3 +1,8 @@
 print("+" + 10*"-" + "+")
 print(("|" + " "*10 + "|\n") *10)
 print("+" + 10*"-" + "+")
+
+#Operators and Expressions
+X = float(input("Enter Your Number to the X: "))
+Y = 1 / (X + 1 / (X + 1 / (X + 1 / X)))
+print(Y)
