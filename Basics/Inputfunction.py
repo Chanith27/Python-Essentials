003e636c3b3031ffa9137880ba4effd01a24c4b6
Chanith Adikari <112007446+Chanith27@users.noreply.github.com>
2025-07-01 10:26:51 +0530

    Update and rename inputfunction.py to Inputfunction.py

---
+++ Basics/Inputfunction.py
@@ -0,0 +1,11 @@
+print("What is your name?")
+name = input()
+print("My name is " + name)
+
+#float inputs
+a = float(input("Enter First Float Number: "))
+b = float(input("Enter Second Float Number: "))
+
+c = a + b
+
+print(float(c))
