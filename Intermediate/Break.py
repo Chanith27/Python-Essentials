f403c29a0a9fa22f47d693bd5322378854db15ee
Chanith Adikari <112007446+Chanith27@users.noreply.github.com>
2025-07-05 12:10:30 +0530

    Create Break.py

---
+++ Intermediate/Break.py
@@ -0,0 +1,6 @@
+while True:
+    word = input("Enter a word (or the secret exit word): ")
+    if word == "chupacabra":   
+        break                  
+
+print("You've successfully left the loop.")
