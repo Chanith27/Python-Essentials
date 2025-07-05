f7262175791fbab74a5c86a4cd2ef487da7ad4ef
Chanith Adikari <112007446+Chanith27@users.noreply.github.com>
2025-07-05 11:20:09 +0530

    Update for_range.py

--- Intermediate/for_range.py
+++ Intermediate/for_range.py
@@ -4,3 +4,9 @@ for i in range(10): #here the range is discribed
 for x in range(2,8,3): #here the starting point, range and gap discribed
     print(x);
 
+import time
+
+for x in range(1,6):
+    print(x, " Mississippi")
+print("Ready or not, here I come!")
+
