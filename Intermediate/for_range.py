a42d880fc89a835b4c173f7854927f78c80264fe
Chanith Adikari <112007446+Chanith27@users.noreply.github.com>
2025-07-05 10:32:57 +0530

    Update for_range.py

--- Intermediate/for_range.py
+++ Intermediate/for_range.py
@@ -1,3 +1,6 @@
-for i in range(10):
+for i in range(10): #here the range is discribed
     print("The value of i is currently", i)
+
+for x in range(2,8,3): #here the starting point, range and gap discribed
+    print(x);
 
