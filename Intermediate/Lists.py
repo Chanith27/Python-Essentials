7bcfa91f8736afaaea326bb2a5e86f4f630eaeb9
Chanith Adikari <112007446+Chanith27@users.noreply.github.com>
2025-07-06 23:01:13 +0530

    Update Lists.py

--- Intermediate/Lists.py
+++ Intermediate/Lists.py
@@ -7,3 +7,19 @@ del numbers[4]
 len(numbers)
 
 print(numbers)
+
+#Here we just get the length of the list
+numbers = [111, 7, 2, 1]
+print(len(numbers))
+print(numbers)
+
+#We add 4 for the end of the list
+numbers.append(4)
+print(len(numbers))
+print(numbers)
+
+#We insert the index, then number we want to add
+numbers.insert(0, 222)
+print(len(numbers))
+print(numbers)
+
