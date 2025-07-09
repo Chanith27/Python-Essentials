82d0fb2373a587aa297b71d1088683a991822eaf
Chanith Adikari <amradikari67@gmail.com>
2025-07-09 10:24:29 +0530

    Create Returnfunction.py

---
+++ Intermediate/Returnfunction.py
@@ -0,0 +1,16 @@
+def wishes():
+    print("My Wishes")
+    return "Happy Birthday"
+
+wishes()    # outputs: My Wishes
+
+
+def wishes():
+    print("My Wishes")
+    return "Happy Birthday"
+
+print(wishes())
+
+# outputs: My Wishes
+#          Happy Birthday
+
