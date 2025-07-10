1d78bccb78a0b34ef450003e4e5630e99760b415
Chanith Adikari <amradikari67@gmail.com>
2025-07-10 09:43:44 +0530

    Create tempCodeRunnerFile.py

---
+++ Intermediate/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+vehicles_one = ['car', 'bicycle', 'motor']
+print(vehicles_one) # outputs: ['car', 'bicycle', 'motor']
+
+vehicles_two = vehicles_one
+del vehicles_one[0] # deletes 'car'
+print(vehicles_two) # outputs: ['bicycle', 'motor']
