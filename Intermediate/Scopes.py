63b16404caa50d03dcfd2c6b54a5fa5c29ed8ab1
Chanith Adikari <amradikari67@gmail.com>
2025-07-11 22:43:18 +0530

    Update Scopes.py

--- Intermediate/Scopes.py
+++ Intermediate/Scopes.py
@@ -36,3 +36,18 @@ def adding(x):
 print(adding(4))    # outputs: 11
 print(var)    # NameError
 
+#Example 5
+
+var = 2
+print(var)    # outputs: 2
+
+
+def return_var():
+    global var
+    var = 5
+    return var
+
+
+print(return_var())    # outputs: 5
+print(var)    # outputs: 5
+
