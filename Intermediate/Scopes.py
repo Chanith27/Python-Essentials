438d1145e189549b9225f15edd317c7dbca305bd
Chanith Adikari <amradikari67@gmail.com>
2025-07-11 22:42:22 +0530

    Update Scopes.py

--- Intermediate/Scopes.py
+++ Intermediate/Scopes.py
@@ -8,3 +8,31 @@ def mult_by_var(x):
 print(mult_by_var(7))    # outputs: 14
 
 #Example 2
+
+def mult(x):
+    var = 5
+    return x * var
+
+
+print(mult(7))    # outputs: 35
+
+#Example 3
+
+def mult(x):
+    var = 7
+    return x * var
+
+
+var = 3
+print(mult(7))    # outputs: 49
+
+#Example 4
+
+def adding(x):
+    var = 7
+    return x + var
+
+
+print(adding(4))    # outputs: 11
+print(var)    # NameError
+
